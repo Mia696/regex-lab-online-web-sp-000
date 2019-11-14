ab7f9c1d7f182d8f52ae54a87c526a927df7a27f
Mia <tamiapaschall@gmail.com>
2019-11-14 20:39:12 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,13 +7,13 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-\b\w[A-Z]{5}\b
+/b\w[A-Z]{5}\b/
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-/^[A-Z]
+
 end
 
 def valid_phone_number?(phone)
-\(\d{3}\) \d{3}-\d{4}
+/\(\d{3}\) \d{3}-\d{4}/
 end
