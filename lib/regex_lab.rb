a32478510f6c46c6c9adc3e620e16cf4f66f0f77
Mia <tamiapaschall@gmail.com>
2019-11-14 21:10:49 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,7 +14,11 @@ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-text.match(/^[A-Z].+[\.!?]$/)
+if text.match(/^[A-Z].+[\.!?]$/)
+  true
+else
+  false
+end
 end
 
 # def valid_phone_number?(phone)
