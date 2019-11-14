103940c84ee0c11d0426aeb1431c0a7ba4dfd5e4
Mia <tamiapaschall@gmail.com>
2019-11-14 20:16:25 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,13 @@
 def starts_with_a_vowel?(word)
-(/^[aeiouAEIOU]\w+/)
+/^[aeiouAEIOU]\w+/
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  (/\b[un}*\b[ing]/)
+  /\b[un}*\b[ing]/
 end
 
 def words_five_letters_long(text)
-(\b\w[A-Z]{5}\b)
+\b\w[A-Z]{5}\b
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
