ca6075ee7d9d3f978128bb251cb6fd134361fe16
Mia <tamiapaschall@gmail.com>
2019-11-14 20:54:15 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -2,18 +2,18 @@ def starts_with_a_vowel?(word)
 /^[aeiouAEIOU]\w+/
 end
 
-def words_starting_with_un_and_ending_with_ing(text)
-  /\b[un]*\b[ing]/
-end
+# def words_starting_with_un_and_ending_with_ing(text)
+#   /\b[un]*\b[ing]/
+# end
 
-def words_five_letters_long(text)
-/\b\w[A-Z]{5}\b/
-end
+# def words_five_letters_long(text)
+# /\b\w[A-Z]{5}\b/
+# end
 
-def first_word_capitalized_and_ends_with_punctuation?(text)
-/\w^[A-Z]\w[?]/
-end
+# def first_word_capitalized_and_ends_with_punctuation?(text)
+# /\w^[A-Z]\w[?]/
+# end
 
-def valid_phone_number?(phone)
-/(\d{3}\) \d{3}-\d{4}/
-end
+# def valid_phone_number?(phone)
+# /(\d{3}\) \d{3}-\d{4}/
+# end
