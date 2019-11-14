2c452451e84deaab9a3032e786b86a5156fd9c27
Mia <tamiapaschall@gmail.com>
2019-11-14 21:14:54 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -22,5 +22,9 @@ end
 end
 
 def valid_phone_number?(phone)
-# /(\d{3}\) \d{3}-\d{4}/
+if phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/)
+  true
+else
+  false
+end
 end
