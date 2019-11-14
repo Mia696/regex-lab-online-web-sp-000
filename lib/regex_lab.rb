4523182f9a9b87d22936e7f6a48567b346d1436e
Mia <tamiapaschall@gmail.com>
2019-11-14 21:08:23 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -13,9 +13,9 @@ def words_five_letters_long(text)
 text.scan(/\b\w{5}\b/)
 end
 
-# def first_word_capitalized_and_ends_with_punctuation?(text)
-# /\w^[A-Z]\w[?]/
-# end
+def first_word_capitalized_and_ends_with_punctuation?(text)
+/\w^[A-Z]\w[?]/
+end
 
 # def valid_phone_number?(phone)
 # /(\d{3}\) \d{3}-\d{4}/
